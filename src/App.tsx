5463d5461ade768284e40071f8527e89a277060d
[dyad] <git@dyad.sh>
2025-09-04 14:13:47 +0100

    [dyad] Setting up React application structure - wrote 4 file(s)

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      {/* This will be where your React components will live */}
+      <h1 className="text-3xl font-bold text-center p-8">Welcome to MyCV.i.ng (React Version)!</h1>
+      <p className="text-center text-gray-600">The migration to React has begun. Let's start building components!</p>
+    </div>
+  );
+}
+
+export default App;
